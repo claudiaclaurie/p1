ba4d988738bc24e267e36b7544e73a63e1590256
Claudia Laurie <claudialaurie@college.harvard.edu>
2017-02-24 04:23:34 +0000

    created header correctly

---
+++ graphgeneration.h
@@ -0,0 +1,10 @@
+
+#ifndef __GRAPHGENERATION__
+#define __GRAPHGENERATION__
+
+extern REM_EDGES;
+
+void g_free(edge** g, int numpoints);
+edge **generate_graph(int numpoints, int dimension, graph_node* ptarray);
+
+#endif 
