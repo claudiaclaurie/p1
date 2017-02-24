c5962231b79ad0c42cced4e01558ad0d2e07e715
Claudia Laurie <claudialaurie@college.harvard.edu>
2017-02-24 04:24:54 +0000

    fixed

---
+++ prims1.h
@@ -0,0 +1,28 @@
+#ifndef __PRIMS1_H__
+#define __PRIMS1_H__
+
+// graph point
+typedef struct graph_node {
+	float a;
+	float b;
+	float c;
+	float d;
+}graph_node;
+
+
+// edge between points
+typedef struct edge {
+	int source;
+	int target;
+	float weight;
+	struct edge* next;		
+}edge;
+
+// heap to store edges, for Prim's algorithm
+typedef struct heap {
+	unsigned int heapsz;
+	edge* heap;
+}heap;
+
+
+#endif
